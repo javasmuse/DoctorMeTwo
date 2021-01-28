c6918f6c3561d6e91df9756529f8e8932840c070
paul <pxl34232@gmail.com>
2021-01-28 15:30:50 -0800

    some refractoring

--- src/app/DoctorMeApp.java
+++ src/app/DoctorMeApp.java
@@ -2,7 +2,7 @@ package app;
 
 import java.io.File;
 
-import entities.Player;
+//import app.XMLController;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -12,66 +12,44 @@ import javax.xml.parsers.DocumentBuilder;
 import java.util.Scanner;
 
 
-public class DoctorMeApp{
-    private Player player;
 
+public class DoctorMeApp{
+    //private Player player;
+    private int HEALTHVALUE = 100; // initiates health value
+    public static final String BOLD_RED = "\033[1;91m";
+
+    private StringBuilder sb = new StringBuilder();
     private Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-
-        //TODO Delegate this behavior to Game Class
         DoctorMeApp app = new DoctorMeApp();
         app.playDrMe();
-
-        //Game game = new Game();
-        //game.start()
 
     }
 
     public void playDrMe() {
+
         System.out.println("Hello model.Player, What is you name?");
-        String playerName = sc.nextLine();
+        String playerName = sc.nextLine().strip();
+        // initialize player
+        //player(String playerName, int HEALTHVALUE);
 
-        System.out.println("Test: the players name is:  " + playerName);
-
-
-        try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            File inputFile = new File("resources/test.xml");
-            Document doc = db.parse(inputFile);
-            doc.getDocumentElement().normalize();
-            System.out.println("Root Element:  " + doc.getDocumentElement().getNodeName());
-            NodeList nodeList = doc.getElementsByTagName("disease");
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                System.out.println("\nNode Name :" + node.getNodeName());
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) node;
-                    System.out.println("Name: " + eElement.getElementsByTagName("name").item(0).getTextContent());
-                    System.out.println("Question: " + eElement.getElementsByTagName("question").item(0).getTextContent());
-                    System.out.println("Points: " + eElement.getElementsByTagName("points").item(0).getTextContent());
-
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        System.out.println("Hello welcome to Dr Me "+ BOLD_RED + playerName );
+        XMLController xmlc = new XMLController();
+        xmlc.readXML();
 
     }
 
-    public void into(){
-
-    }
-
-    public void getUserInput(){
-
-    }
-
-    public void getXML(){
-
-    }
+//    public void into(){
+//
+//    }
+//
+//    public void getUserInput(){
+//
+//    }
+//
+//    public void getXML(){
+//
+//    }
 
 }
--- src/app/XMLController.java
+++ src/app/XMLController.java
@@ -1,37 +1,66 @@
 package app;
 
-import entities.Threat;
+//import entities.Threat;
 
 import java.util.Collection;
-
+import java.io.File;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import java.util.*;
 
 public class XMLController {
+   // XMLController xmlc = new XMLController();
+
+    private List<Object> diseases = new ArrayList<>();
 
     private String xmlFileName;
 
-    public XMLController(String xmlFileName) {
-        this.xmlFileName = xmlFileName;
+    public XMLController() {
     }
-
     // read xml, not sure if this is needed, maybe just use as private
-    public void readXML(String fileName){
+    public void readXML(){
         // push into collection
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            File inputFile = new File("resources/test.xml");
+            Document doc = db.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root Element:  " + doc.getDocumentElement().getNodeName());
+            NodeList nodeList = doc.getElementsByTagName("disease");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                System.out.println("\nNode Name :" + node.getNodeName());
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    System.out.println("Name: " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    System.out.println("Question: " + eElement.getElementsByTagName("question").item(0).getTextContent());
+                    System.out.println("Points: " + eElement.getElementsByTagName("points").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
-
-
     // Parse entities
-    public Collection<Threat> parseXML(){
-        //readXML(this.filename)
-        return null;
-    }
-
-    public String getXmlFileName() {
-        return xmlFileName;
-    }
-
-    public void setXmlFileName(String xmlFileName) {
-        this.xmlFileName = xmlFileName;
-    }
+//    public Collection<Threat> parseXML(){
+//        //readXML(this.filename)
+//        return null;
+//    }
+//    public String getXmlFileName() {
+//        return xmlFileName;
+//    }
+//    public void setXmlFileName(String xmlFileName) {
+//        this.xmlFileName = xmlFileName;
+//    }
 
 
 
