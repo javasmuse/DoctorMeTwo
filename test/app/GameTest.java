3585f0ce88a73b207c08d42792b96cc28f69f5c5
nscherer30 <nscherer30@gmail.com>
2021-01-28 13:36:24 -0800

    starting test classes and headers created

---
+++ test/app/GameTest.java
@@ -0,0 +1,30 @@
+package app;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void play() {
+    }
+
+    @Test
+    public void playAgain() {
+    }
+}
