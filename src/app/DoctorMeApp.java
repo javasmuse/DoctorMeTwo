c2cc4315458f174435b975fa00873ba4f784bb2a
nscherer30 <nscherer30@gmail.com>
2021-01-29 10:58:56 -0800

    first version of game introduction, describes commands and adventurous game introduction

--- src/app/DoctorMeApp.java
+++ src/app/DoctorMeApp.java
@@ -14,9 +14,25 @@ import java.util.Scanner;
 
 
 public class DoctorMeApp{
-    //private Player player;
+    private final String GAME_INTRODUCTION =
+            "Doctor Me needs your help!\n" +
+            "You are on a mission to save the body\n" +
+            "Here are some tips on how to play...\n" +
+            "Commands:\n" +
+            "HELP - displays more information on commands\n" +
+            "HINT - gives you a hint on what to do next\n" +
+            "GET <item> - gets the item from your inventory to see if its useful\n" +
+            "INVENTORY - displays the items you have";
+
+    private final String GAME_INTRODUCTION_TWO =
+            "The walls shake as the roar of borborygmus envelops the room.\n" +
+            "As you look around, you notice the walls have many folds--rugae, perhaps?\n" +
+            "The floor bubbling with acid, you realize you're in the stomach.\n" +
+            "What discoveries await as you explore the inner-workings of the human body?";
+
     private int HEALTHVALUE = 100; // initiates health value
     public static final String BOLD_RED = "\033[1;91m";
+    //private Player player;
 
     private StringBuilder sb = new StringBuilder();
     private Scanner sc = new Scanner(System.in);
@@ -34,7 +50,10 @@ public class DoctorMeApp{
         // initialize player
         //player(String playerName, int HEALTHVALUE);
 
+        // Display game introduction related information
         System.out.println("Hello welcome to Dr Me "+ BOLD_RED + playerName );
+        System.out.println(GAME_INTRODUCTION);
+        System.out.println(GAME_INTRODUCTION_TWO);
         XMLController xmlc = new XMLController();
         xmlc.readXML();
 
