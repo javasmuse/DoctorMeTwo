c14b74b63dd280934a35495739ef4daba1908420
bradapratt <bradapratt@gmail.com>
2021-02-10 08:52:44 -0800

    Finished V1 implementation of Badge.java and added javadoc.

--- src/com/doctorme/entities/Badge.java
+++ src/com/doctorme/entities/Badge.java
@@ -1,20 +1,46 @@
 package com.doctorme.entities;
+/**
+ * Badge.java implements Badges that the player can collect
+ * as they progress through the game. Unlocking all the badges
+ * allows them to access the final level and attempt to win the
+ * game. Badges have two attributes: a name (String) and an image
+ * file (String).
+ * <p>
+ * Author: Bradley Pratt
+ * Last Edited: 02/10/2021
+ */
 
 public class Badge {
     private String name;
+    //in string format so GUI can load the file
+    private String imageFile;
+
     public Badge(){
-        //default constructor
+        setName("NULL");
     }
 
     public Badge(String name) {
-        this.name = name;
+        setName(name);
+    }
+
+    public Badge(String name, String imageFile) {
+        setName(name);
+        setImageFile(imageFile);
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
+    }
+
+    public String getImageFile() {
+        return imageFile;
+    }
+
+    private void setImageFile(String imageFile) {
+        this.imageFile = imageFile;
     }
 }
